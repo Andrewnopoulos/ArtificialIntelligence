142d40a8ed7d07b3ca1b6cff2cb61776ec831a47
Andrew <andrew.giannopoulos@gmail.com>
2015-05-01 17:21:10 +1000

    Added bitfields to checkers class

--- Neural/NeuralNet/NeuralNet/Checkers.cpp
+++ Neural/NeuralNet/NeuralNet/Checkers.cpp
@@ -3,11 +3,5 @@
 
 Checkers::Checkers()
 {
-	m_board = new int*[8];
-	for (int i = 0; i < 8; i++)
-	{
-		m_board[i] = new int[8];
-	}
 
-	memset(m_board, 0, sizeof(int)* 64);
 }
--- Neural/NeuralNet/NeuralNet/Checkers.h
+++ Neural/NeuralNet/NeuralNet/Checkers.h
@@ -1,10 +1,19 @@
 #pragma once
 
+struct Board
+{
+	unsigned int m_WhitePieces : 5; // 32 bits
+	unsigned int m_BlackPieces : 5;
+	unsigned int m_WhiteKings : 5;
+	unsigned int m_BlackKings : 5;
+};
+
 class Checkers
 {
 
 private:
-	int** m_board;
+	Board m_board;
+	
 
 
 public:
